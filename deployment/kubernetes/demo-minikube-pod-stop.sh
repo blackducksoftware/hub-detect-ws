8627876cb6fee3c974c95b01d989eb3a285bdc01
Steve Billings <sbillings@blackducksoftware.com>
2018-05-07 17:17:01 -0400

    separate detect and imageinspector pods

--- deployment/kubernetes/demo-minikube-pod-stop.sh
+++ deployment/kubernetes/demo-minikube-pod-stop.sh
@@ -9,6 +9,11 @@ echo "--------------------------------------------------------------"
 echo "Deleting service, pod, configmap, namespace"
 echo "--------------------------------------------------------------"
 kubectl delete service "${serviceName}" --namespace ${nameSpace}
+kubectl delete service hub-imageinspector-ws --namespace ${nameSpace}
 kubectl delete deployment "${deploymentName}" --namespace ${nameSpace}
+kubectl delete deployment hub-imageinspector-ws --namespace ${nameSpace}
+#
+# Usually don't want to delete these
+#
 ##kubectl delete configmap "${configMapName}" --namespace ${nameSpace}
 ##kubectl delete namespace "${nameSpace}"
